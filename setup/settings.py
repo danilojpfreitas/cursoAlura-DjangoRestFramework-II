f3222e190d6fe24c33aba9ee57e7f27f57294f94
danilojpfreitas <danilojpfreitas@gmail.com>
2022-09-01 20:39:23 -0300

    Version 1

--- setup/settings.py
+++ setup/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 15
 }
 
 django_heroku.settings(locals())
